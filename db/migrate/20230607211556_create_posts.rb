23ef99c716f50a215986b64d87bb06aa4e413a8f
Dico Diaz <dicodiaz@gmail.com>
2023-06-07 16:49:00 -0500

    Create db schema

---
+++ db/migrate/20230607211556_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.bigint :author_id
+      t.string :title
+      t.text :text
+
+      t.timestamps
+    end
+    add_index :posts, :author_id
+    add_foreign_key :posts, :users, column: :author_id
+  end
+end
