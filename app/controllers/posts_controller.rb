ddc3e0d2b72d0e0c50e7bbf3f0315e54770cfed2
Dico Diaz <dicodiaz@gmail.com>
2023-06-14 16:51:50 -0500

    Implement post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,19 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :text))
+    @post.author = current_user
+    if @post.save
+      redirect_to user_post_url(current_user, @post.id), flash: { success: 'Post saved successfully' }
+    else
+      flash.now[:error] = @post.errors.full_messages.to_sentence.prepend('Error(s): ')
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
