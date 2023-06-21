d11640e1baee7bbcb7ef056706b398f38e2d9543
Dico Diaz <dicodiaz@gmail.com>
2023-06-21 14:55:38 -0500

    Add pagination and suggested changes

--- spec/system/posts/index_spec.rb
+++ spec/system/posts/index_spec.rb
@@ -57,47 +57,17 @@ RSpec.describe Post, type: :system do
       expect(page).to have_content("Comments: #{@post.likes.size}")
     end
 
+    it 'displays a section for pagination if there are more than 2 posts' do
+      expect(page).not_to have_content('Next')
+      post2 = Post.create(author: @user, title: 'post2 title', text: 'post2 text')
+      post3 = Post.create(author: @user, title: 'post3 title', text: 'post3 text')
+      refresh
+      expect(page).to have_content('Next')
+    end
+
     it "redirects to a post's show page when clicking on it" do
       click_link(@post.title)
       expect(page).to have_current_path(user_post_path(@user, @post))
-    end
-  end
-
-  describe 'show page' do
-    before(:each) do
-      @user2 = User.create(name: 'user2 name', photo: '/assets/placeholder2.png', bio: 'user2 bio')
-      @comment2 = Comment.create(author: @user2, post: @post, text: 'comment2 text')
-      visit user_post_path(@user, @post)
-    end
-
-    it "displays the post's title" do
-      expect(page).to have_content(@post.title)
-    end
-
-    it 'displays who wrote the post' do
-      expect(page).to have_content(@user.name)
-    end
-
-    it 'displays how many comments the post has' do
-      expect(page).to have_content("Comments: #{@post.comments.size}")
-    end
-
-    it 'displays how many likes the post has' do
-      expect(page).to have_content("Likes: #{@post.likes.size}")
-    end
-
-    it "displays the post's body" do
-      expect(page).to have_content(@post.text)
-    end
-
-    it 'displays the username of each commentor' do
-      expect(page).to have_content(@comment.author.name)
-      expect(page).to have_content(@comment2.author.name)
-    end
-
-    it 'displays the comment each commentor left' do
-      expect(page).to have_content(@comment.text)
-      expect(page).to have_content(@comment2.text)
     end
   end
 end
--- spec/system/users/show_spec.rb
+++ spec/system/users/show_spec.rb
@@ -6,32 +6,6 @@ RSpec.describe User, type: :system do
     @post = Post.create(author: @user, title: 'post title', text: 'post text')
   end
 
-  describe 'index page' do
-    before(:each) do
-      @user2 = User.create(name: 'user2 name', photo: '/assets/placeholder2.png', bio: 'user2 bio')
-      visit users_path
-    end
-
-    it 'displays the username for all users' do
-      expect(page).to have_content(@user.name)
-      expect(page).to have_content(@user2.name)
-    end
-
-    it 'displays the profile picture for each user' do
-      expect(page).to have_css("img[src*='#{@user.photo}']")
-      expect(page).to have_css("img[src*='#{@user2.photo}']")
-    end
-
-    it 'displays the number of posts each user has written' do
-      expect(page).to have_content("Number of posts: #{@user.posts.size}")
-    end
-
-    it "redirects to a user's show page when clicking on it" do
-      click_link(@user.name)
-      expect(page).to have_current_path(user_path(@user))
-    end
-  end
-
   describe 'show page' do
     before(:each) do
       visit user_path(@user)
