3a5eb488d371ff9759c2316fb8710bcdbc081c3e
Dico Diaz <dicodiaz@gmail.com>
2023-06-25 00:08:39 -0500

    Fix system specs

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,8 +2,36 @@ require 'rails_helper'
 
 RSpec.describe User, type: :system do
   before(:each) do
-    @user = User.create(name: 'user name', photo: '/assets/placeholder.png', bio: 'user bio')
+    @user = User.create(name: 'user name', photo: '/assets/placeholder.png', bio: 'user bio',
+                        email: 'user_mail@mail.com', password: 'user_password')
     @post = Post.create(author: @user, title: 'post title', text: 'post text')
+  end
+
+  describe 'index page' do
+    before(:each) do
+      @user2 = User.create(name: 'user2 name', photo: '/assets/placeholder2.png', bio: 'user2 bio',
+                           email: 'user2_mail@mail.com', password: 'user2_password')
+      visit users_path
+    end
+
+    it 'displays the username for all users' do
+      expect(page).to have_content(@user.name)
+      expect(page).to have_content(@user2.name)
+    end
+
+    it 'displays the profile picture for each user' do
+      expect(page).to have_css("img[src*='#{@user.photo}']")
+      expect(page).to have_css("img[src*='#{@user2.photo}']")
+    end
+
+    it 'displays the number of posts each user has written' do
+      expect(page).to have_content("Number of posts: #{@user.posts.size}")
+    end
+
+    it "redirects to a user's show page when clicking on it" do
+      click_link(@user.name)
+      expect(page).to have_current_path(user_path(@user))
+    end
   end
 
   describe 'show page' do
